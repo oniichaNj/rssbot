135f359e4a7660c50f920a2fb21cab02ddde126d
oniichaNj <oniichan@mrowr.me>
2016-03-27 15:13:36 +0200

    rss

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/thoj/go-ircevent"
+	"net/http"
 	"os"
+	"time"
 	"encoding/json"
 	"crypto/tls"
 	"strings"
 	"github.com/SlyMarbo/rss"
+	"io/ioutil"
 )
 
 type Config struct {
@@ -76,20 +79,79 @@ func main() {
 	conn.AddCallback("PRIVMSG", func(e *irc.Event) {
 		go func(e *irc.Event){
 			if strings.HasPrefix(e.Message(), config.Prefix) {
+				if stringInSlice(e.Arguments[0], config.Channels) {
+					for i, _ := range feeds {
+						feeds[i].Update()
+					}
+					// beware, magic numbers
+					for _, feed := range feeds {
+						if feed.Unread < 5 {
+							for i, _ := range feed.Items {
+								s, err := urlshorten(feed.Items[i].Link)
+								if err != nil {
+									fmt.Println("Couldn't shorten url: ", err)
+								} else {
+									conn.Privmsg(e.Arguments[0], feed.Items[i].Title + " - " + s)
+								}
+							}
+						}
+					}
+				}
+			}
+		}(e)
+	});
+
+	ticker := time.NewTicker(3 * time.Minute)
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <- ticker.C:
 				for i, _ := range feeds {
 					feeds[i].Update()
 				}
 				for _, feed := range feeds {
-					for i, _ := range feed.Items {
-						fmt.Println(feed.Items[i].Title + " - " + feed.Items[i].Link)
+					if feed.Unread < 10 {
+						for i, _ := range feed.Items {
+							for _, channel := range config.Channels {
+								s, err := urlshorten(feed.Items[i].Link)
+								if err != nil {
+									fmt.Println("Couldn't shorten url: ", err)
+								} else {
+									conn.Privmsg(channel, feed.Items[i].Title + " - " + s)
+								}
+							}
+						}
 					}
-				}
+				}		
+			case <- quit:
+				ticker.Stop()
+				return
 			}
-		//event.Message() contains the message
-		//event.Nick Contains the sender
-		//event.Arguments[0] Contains the channel
-		}(e)
-	});
-
+		}
+	}()
 	conn.Loop()
+}
+
+func urlshorten(url string) (string, error) {
+	//https://is.gd/create.php?format=simple&url=www.example.com
+	resp, err := http.Get("https://is.gd/create.php?format=simple&url=" + url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
